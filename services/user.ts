f03f3af819cb07b24d1dee32c29bc921c99eb21a
Mahdi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2023-07-12 10:34:58 +0330

    fix: fix ts types

--- services/user.ts
+++ services/user.ts
@@ -5,11 +5,11 @@ import { messages } from "@/messages";
 export const userSchema = z.object({
 	email: z
 		.string()
-		.min(1, { message: messages.register.master.validation.email.min })
+		.nonempty(messages.register.master.validation.email.require)
 		.email("ایمیل نامعتبر است"),
 	password: z
 		.string()
-		.min(1, { message: messages.register.master.validation.password.min }),
+		.nonempty(messages.register.master.validation.password.require),
 });
 
 export type User = z.infer<typeof userSchema>;
